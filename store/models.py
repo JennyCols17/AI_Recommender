555fdcb0d7a2b3cf08a41027ba8b93404382668b
Angelo Bernardo <dhayn101@gmail.com>
2025-04-28 21:51:52 +0800

    Initial commit: Set up Django project with MongoDB and AI recommender

---
+++ store/models.py
@@ -0,0 +1,11 @@
+from djongo import models
+
+class Product(models.Model):
+    _id = models.ObjectIdField()
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
